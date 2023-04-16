38f5b6877f8197c496cf2adb430dc565443237f6
Перепелкин Илья <perepelkin_ilya@mail.ru>
2023-04-16 13:46:03 +0300

    Выполнил задание 12.1

--- src/Card/IPaySystem/IPaySystem.java
+++ src/Card/IPaySystem/IPaySystem.java
@@ -2,10 +2,10 @@ package Card.IPaySystem;
 
 public interface IPaySystem {
 
-    float sumInBillingCurrency = 0;
-    
-    // Конвертировать в валюту по курсу платежной системы
     default float convertToCurrencyExchangeRatePaySystem(float sum, String fromCurrencyCode, String toBillingCurrencyCode) {
+        float exchangeRateCurrencyToBillingCurrency = getExchangeRatePaySystem(fromCurrencyCode, toBillingCurrencyCode);
+        float sumInBillingCurrency = sum * exchangeRateCurrencyToBillingCurrency;
+
         return sumInBillingCurrency;
     }
 
