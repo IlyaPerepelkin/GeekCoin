debe4b78587b61d9adc0d3c0a950ab2cb369629c
Перепелкин Илья <perepelkin_ilya@mail.ru>
2023-06-09 15:33:52 +0300

    Добавил курсы валют платёжных систем

--- src/ru/geekstar/Card/CardVisa.java
+++ src/ru/geekstar/Card/CardVisa.java
@@ -17,8 +17,9 @@ public abstract class CardVisa extends Card implements IVisa {
     public String getCurrencyCodePaySystem(String country) {
         // по умолчанию null, потому что не во всех странах может использоваться данная платежная система
         String billingCurrencyCode = null;
-        // если покупка в Турции, то валюта биллинга в $
-        if (country.equalsIgnoreCase("Турция")) billingCurrencyCode = CURRENCY_CODE_PAY_SYSTEM_USD;
+        // если покупка в Турции или Казахстане или Франции, то валюта биллинга в $
+        if (country.equalsIgnoreCase("Турция") || country.equalsIgnoreCase("Казахстан")
+                || country.equalsIgnoreCase("Франция")) billingCurrencyCode = CURRENCY_CODE_PAY_SYSTEM_USD;
 
         return billingCurrencyCode;
     }
@@ -27,11 +28,25 @@ public abstract class CardVisa extends Card implements IVisa {
     public ArrayList<Float> getExchangeRatePaySystem(String currency, String currencyExchangeRate) {
         // TODO: Запрос к API Visa
         ArrayList<Float> exchangeRatePaySystem = new ArrayList<>();
-        // курс лиры к доллару
+
+        // курс лиры в долларах
         if (currency.equals("TRY") && currencyExchangeRate.equals("USD")) {
-            exchangeRatePaySystem.add(0.056f); // курс покупки
-            exchangeRatePaySystem.add(0.046f); // курс продажи
+            exchangeRatePaySystem.add(0.0448f); // курс покупки
+            exchangeRatePaySystem.add(0.0496f); // курс продажи
         }
+
+        // курс тенге в долларах
+        if (currency.equals("KZT") && currencyExchangeRate.equals("USD")) {
+            exchangeRatePaySystem.add(0.0021f);
+            exchangeRatePaySystem.add(0.0023f);
+        }
+
+        // курс евро в долларах
+        if (currency.equals("EUR") && currencyExchangeRate.equals("USD")) {
+            exchangeRatePaySystem.add(1.024f);
+            exchangeRatePaySystem.add(1.132f);
+        }
+
         return exchangeRatePaySystem;
     }
 
